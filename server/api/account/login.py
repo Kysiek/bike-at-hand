c57990c51a2fe60d11306d05d9d4b302da718b5b
Miłosz Szymczak <szymczak.milosz@gmail.com>
2015-11-04 14:30:56 +0000

    [Python] #26 Used post method from requests module in login method

--- server/api/account/login.py
+++ server/api/account/login.py
@@ -1,11 +1,11 @@
-from config.constants import NB_API_LOGIN_ACTION, NB_API_LOGIN_LOGIN, NB_API_LOGIN_USERNAME, NB_API_LOGIN_PASSWORD, API_COOKIE_NAME, SESSION_AUTH_TOKEN, SESSION_USERNAME, SESSION_PASSWORD
-from web.http_requests import post
+from config.constants import API_URL, NB_API_LOGIN_ACTION, NB_API_LOGIN_LOGIN, NB_API_LOGIN_USERNAME, NB_API_LOGIN_PASSWORD, API_COOKIE_NAME, SESSION_AUTH_TOKEN, SESSION_USERNAME, SESSION_PASSWORD
 from parsers.html_data import is_logged
 from flask import session
+import requests
 
 
 def login_to_api(username, password):
-    response = post({NB_API_LOGIN_ACTION: NB_API_LOGIN_LOGIN, NB_API_LOGIN_USERNAME: username, NB_API_LOGIN_PASSWORD: password})
+    response = requests.post(API_URL, data={NB_API_LOGIN_ACTION: NB_API_LOGIN_LOGIN, NB_API_LOGIN_USERNAME: username, NB_API_LOGIN_PASSWORD: password})
     if is_logged(response.text) and API_COOKIE_NAME in response.cookies:
         session[SESSION_AUTH_TOKEN] = response.cookies[API_COOKIE_NAME]
         session[SESSION_USERNAME] = username
